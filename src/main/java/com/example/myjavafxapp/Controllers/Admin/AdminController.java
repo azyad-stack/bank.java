378b14d07d950403c1764329b4e8ef0226b228c9
blvckzetsu0 <dzekod109@gmail.com>
2025-11-27 11:00:33 +0100

    changes has been done

--- src/main/java/com/example/myjavafxapp/Controllers/Admin/AdminController.java
+++ src/main/java/com/example/myjavafxapp/Controllers/Admin/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.myjavafxapp.Controllers.Admin;
 
 import com.example.myjavafxapp.Models.Model;
+import com.example.myjavafxapp.Views.AdminMenuOptions;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
@@ -10,12 +11,28 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminController implements Initializable {
+
+    @FXML
     private BorderPane admin_parent;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        Model.getInstance().getViewFactory().getadminSelectedMenuItem().addListener((observableValue,oldVal,newVal) -> {
-            //ADD Switch statement
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().addListener((observableValue, oldVal, newVal) -> {
+
+            // Example switch — you will customize this later
+            switch (newVal) {
+                case CREATE_CLIENT:
+                    AnchorPane view = Model.getInstance().getViewFactory().getCreateClientView();
+                    admin_parent.setCenter(view);
+                    break;
+
+                // Add your other menu options here…
+
+                default:
+                    break;
+            }
+
         });
     }
-
 }
