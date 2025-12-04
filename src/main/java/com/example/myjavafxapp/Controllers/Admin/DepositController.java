054afe85e2f6b6dfd31fe692bf52bf59526180fe
ADMIN <a.zyad@edu.umi.ac.ma>
2025-12-04 21:37:12 +0100

    done

--- src/main/java/com/example/myjavafxapp/Controllers/Admin/DepositController.java
+++ src/main/java/com/example/myjavafxapp/Controllers/Admin/DepositController.java
@@ -1,11 +1,21 @@
 package com.example.myjavafxapp.Controllers.Admin;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepositController implements Initializable {
+
+    public TextField pAddress_fld;
+    public FontAwesomeIconView PaddressSearch_btn;
+    public ListView Paddress_lisview;
+    public TextField Amount_fld;
+    public Button Deposit_btn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
