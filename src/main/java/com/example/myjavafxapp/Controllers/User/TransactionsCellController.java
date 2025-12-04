5fdb760c3a5be3273b6e8ef071f0cbf319385a26
ADMIN <a.zyad@edu.umi.ac.ma>
2025-12-04 22:43:20 +0100

    add Transactions Cell

--- src/main/java/com/example/myjavafxapp/Controllers/User/TransactionsCellController.java
+++ src/main/java/com/example/myjavafxapp/Controllers/User/TransactionsCellController.java
@@ -1,11 +1,20 @@
 package com.example.myjavafxapp.Controllers.User;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionsCellController implements Initializable {
+    public FontAwesomeIconView left_arrow_trans_icon;
+    public FontAwesomeIconView right_arrow_trans_icon;
+    public Label date_trans_lbl;
+    public Label sender_trans_lbl;
+    public Label reciever_trans_lbl;
+    public Label amount_trans_lbl;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
