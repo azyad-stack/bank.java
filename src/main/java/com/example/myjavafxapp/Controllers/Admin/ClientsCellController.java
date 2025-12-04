a6739e2d98988351546e7cbcc2a7808034a34ff5
ADMIN <a.zyad@edu.umi.ac.ma>
2025-12-04 23:28:35 +0100

    Clients Cell Admin Section

---
+++ src/main/java/com/example/myjavafxapp/Controllers/Admin/ClientsCellController.java
@@ -0,0 +1,22 @@
+package com.example.myjavafxapp.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientsCellController implements Initializable {
+
+    public Label name_lbl;
+    public Label lastname_lbl;
+    public Label ch_account_lbl;
+    public Label sv_account_lbl;
+    public Label date_lbl;
+    public Label payeeAddress_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
